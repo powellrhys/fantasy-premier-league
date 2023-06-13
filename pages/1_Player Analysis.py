d79f5bc12c1c95e5d3f93c69c82ab226b0359502
rhys powell <rhys.powell2@btinternet.com>
2023-06-13 17:39:44 +0100

    plots added to page 1

--- pages/1_Player Analysis.py
+++ pages/1_Player Analysis.py
@@ -1,14 +1,30 @@
 import streamlit as st
 
 from pages.functions.support_functions import read_csv
-from pages.functions.plots import plot_cost_to_points
+from pages.functions.plots import plot_cost_to_points, plot_minutes_to_points
 
 st.set_page_config(
     page_title="Player Analysis",
     page_icon=":soccer:",
 )
 
-st.markdown("# Plotting Demo")
+player_data_df = read_csv('data/players.csv')
 
-cost_to_points = plot_cost_to_points()
+st.markdown("# Player Analyis")
+
+option = st.selectbox(
+    'Position',
+    ('All', 'Goalkeeper', 'Defender', 'Midfielder', 'Forward'))
+
+if option == 'All':
+    filter = ['Goalkeeper', 'Defender', 'Midfielder', 'Forward']
+else:
+    filter = [option]
+
+player_data_df = player_data_df[player_data_df['position'].isin(filter)]
+
+cost_to_points = plot_cost_to_points(player_data_df)
 st.plotly_chart(cost_to_points, use_container_width=True)
+
+minutes_to_points = plot_minutes_to_points(player_data_df)
+st.plotly_chart(minutes_to_points, use_container_width=True)
--- pages/functions/plots.py
+++ pages/functions/plots.py
@@ -1,9 +1,7 @@
 from pages.functions.support_functions import read_csv
 import plotly.express as px
 
-def plot_cost_to_points():
-
-    df = read_csv('data/players.csv')
+def plot_cost_to_points(df):
 
     fig = px.scatter(df, x='now_cost', 
                     y='total_points', 
@@ -12,6 +10,22 @@ def plot_cost_to_points():
                     trendline='ols',
                     labels={
                         "now_cost" : "Player Cost",
+                        "total_points" : "Total Points",
+                        "position" : "Position",
+                        "second_name" : "Name"
+                    })
+    
+    return fig
+
+def plot_minutes_to_points(df):
+
+    fig = px.scatter(df, x='minutes', 
+                    y='total_points', 
+                    color='position', 
+                    hover_data='second_name', 
+                    trendline='ols',
+                    labels={
+                        "minutes" : "Minutes Played",
                         "total_points" : "Total Points",
                         "position" : "Position",
                         "second_name" : "Name"
