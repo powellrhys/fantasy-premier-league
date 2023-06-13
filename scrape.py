cbf4874ed492d746ab335ac43cbec754c900ac6e
rhys powell <rhys.powell2@btinternet.com>
2023-06-13 12:32:48 +0100

    refactor

--- scrape.py
+++ scrape.py
@@ -1,20 +1,18 @@
-from data_scraping.fixtures import collect_fixture_data
-from data_scraping.players import collect_player_data
-from data_scraping.league import collect_league_data
+from scraping_functions.fixtures import collect_fixture_data
+from scraping_functions.players import collect_player_data
+from scraping_functions.league import collect_league_data
 
+from dotenv import load_dotenv
 import warnings
 import os
 
 warnings.filterwarnings("ignore")
-
-from dotenv import load_dotenv
-
 load_dotenv()
-
-league_ids = os.getenv('leagues').replace(' ', '').split(',')
 
 collect_fixture_data()
 collect_player_data()
+
+league_ids = os.getenv('leagues').replace(' ', '').split(',')
 
 for league_id in league_ids:
     collect_league_data(league_id)
