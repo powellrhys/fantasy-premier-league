4a6d16aa35c8a8ee7f23ea57b328d8df9b41dfed
rhys powell <rhys.powell2@btinternet.com>
2023-06-14 10:04:10 +0100

    pages cleaned up

--- pages/1_Player Value.py
+++ pages/1_Player Value.py
@@ -1,5 +1,7 @@
 from pages.functions.support_functions import read_csv
-from pages.functions.plots import plot_scatter_player_points
+from pages.functions.plots import \
+    plot_scatter_player_points, \
+    plot_player_points_bar
 
 import streamlit as st
 import numpy as np
@@ -29,10 +31,17 @@ budget_filter = st.sidebar.slider(label='Budget',
                           value=14.0, 
                           step=0.1)
 
-tab1, tab2, tab3 = st.tabs(['Player Cost', 'Minutes Played', 'Player Popularity'])
+tab1, tab2, tab3, tab4, tab5 = st.tabs(['Player Cost', 'Minutes Played', 
+                                        'Player Popularity', 'Top Performers',
+                                        'Top Value Performers'])
 
+player_data_df['points_per_cost'] = player_data_df['total_points'] / player_data_df['now_cost']
 player_data_df = player_data_df[player_data_df['position'].isin(position_filter)]
 player_data_df = player_data_df[player_data_df['now_cost'] <= budget_filter]
+
+top_player_data_df = player_data_df.sort_values(by=['total_points'], ascending=False).head(15)
+top_player_data_df_normalised = player_data_df.sort_values(by=['points_per_cost'], ascending=False).head(15)
+
 
 with tab1:
     cost_to_points = plot_scatter_player_points(player_data_df, 'now_cost', 'Player Cost')
@@ -45,3 +54,11 @@ with tab2:
 with tab3:
     popularity_to_points = plot_scatter_player_points(player_data_df, 'selected_by_percent', 'Selected By (%)')
     st.plotly_chart(popularity_to_points, use_container_width=True)
+
+with tab4:
+    top_value_players = plot_player_points_bar(top_player_data_df, 'total_points', 'Total Points')
+    st.plotly_chart(top_value_players, use_container_width=True)
+
+with tab5:
+    top_value_player_normalised = plot_player_points_bar(top_player_data_df_normalised, 'points_per_cost', 'Points per Unit Cost')
+    st.plotly_chart(top_value_player_normalised, use_container_width=True)
--- pages/2_Team Value.py
+++ pages/2_Team Value.py
@@ -20,19 +20,3 @@ team_data_df = team_data_df[team_data_df['position'].isin(team_position_filter)]
 
 points_per_team = plot_points_per_team(team_data_df)
 st.plotly_chart(points_per_team, use_container_width=True)
-
-player_position_filter = st.selectbox(
-    'Position',
-    ('Goalkeeper', 'Defender', 'Midfielder', 'Forward'))
-
-player_data_df['points_per_cost'] = player_data_df['total_points'] / player_data_df['now_cost']
-player_data_df_filtered = player_data_df[player_data_df['position'] == player_position_filter]
-
-top15_player_data_df_filtered = player_data_df_filtered.sort_values(by=['total_points'], ascending=False).head(15)
-top15_player_data_df_filtered_normalised = player_data_df_filtered.sort_values(by=['points_per_cost'], ascending=False).head(15)
-
-points_per_position = plot_player_points_bar(top15_player_data_df_filtered, 'total_points', 'Total Points')
-st.plotly_chart(points_per_position, use_container_width=True)
-
-points_per_position_normalised = plot_player_points_bar(top15_player_data_df_filtered_normalised, 'points_per_cost', 'Points per Unit Cost')
-st.plotly_chart(points_per_position_normalised, use_container_width=True)
