a30febafc5f3b7eb0e393fa369dbdf0bf6e57e4f
rhys powell <rhys.powell2@btinternet.com>
2023-06-15 10:05:22 +0100

    id column added to players dataframe

--- pages/functions/players.py
+++ pages/functions/players.py
@@ -12,12 +12,12 @@ def collect_player_data():
     elements_types_df = pd.DataFrame(json['element_types'])
     teams_df = pd.DataFrame(json['teams'])
 
-    slim_elements_df = elements_df[['second_name','team','element_type','selected_by_percent','now_cost','minutes','transfers_in','value_season','total_points']]
+    slim_elements_df = elements_df[['id','second_name','team','element_type','selected_by_percent','now_cost','minutes','transfers_in','value_season','total_points']]
     slim_elements_df['position'] = slim_elements_df.element_type.map(elements_types_df.set_index('id').singular_name)
     slim_elements_df['team'] = slim_elements_df.team.map(teams_df.set_index('id').name)
     slim_elements_df['value'] = slim_elements_df.value_season.astype(float)
     slim_elements_df['now_cost'] = slim_elements_df['now_cost'].div(10)
 
-    slim_elements_df = slim_elements_df[['second_name','team','position','selected_by_percent','now_cost','minutes','transfers_in','value_season','total_points']]
+    slim_elements_df = slim_elements_df[['id','second_name','team','position','selected_by_percent','now_cost','minutes','transfers_in','value_season','total_points']]
 
     slim_elements_df.to_csv('data/players.csv')
