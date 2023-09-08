2ec1f8fe3d08402485762142698d0e07b4c4e766
Rhys Powell <55314665+powellrhys@users.noreply.github.com>
2023-09-08 11:25:20 +0100

    big authentication refactor (#6)
    * big authentication refactor
    
    * build pipeline fix

--- pages/3_My Team Analysis.py
+++ pages/3_My Team Analysis.py
@@ -7,7 +7,9 @@ from pages.functions.players import \
 from pages.functions.plots import \
     plot_line_my_team_stats
 
+from dotenv import load_dotenv
 import streamlit as st
+import os
 
 # Configure page config
 st.set_page_config(
@@ -15,56 +17,70 @@ st.set_page_config(
     page_icon=":soccer:",
 ) 
 
-# Collect user team name
-team_name, _, _, _, _ = summarise_manager_data('data/manager_summary.csv')  
+load_dotenv()
 
-# UI components
-st.title(f'{team_name} Analysis')
+col1, col2, = st.columns([3,2])
 
-# Sidebar gameweek slider
-gameweek_range = st.sidebar.slider(
-    'Gameweeks to Analyse',
-    1, 38, (1, 38))
+with col1:
+    # UI components
+    st.title('Team Analysis')
 
-# Siderbar position radio component
-position_radio = st.sidebar.radio(
-    "Position to Analyse",
-    ('Goalkeeper', 'Defender', 'Midfielder', 'Forward'))
+with col2:
+    pw = st.text_input('Password', type='password')
 
-# Collect and filter player data
-players_df = read_csv('data/players.csv')
-player_filter = players_df[players_df['position'] == position_radio]['second_name'].to_list()
+if pw != os.getenv('password'):
 
-# Configure page tabs
-tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(['GW Points', 'GW Minutes', 'GW Goals', 'GW Assists', 
-                                        'GW CS', 'GW Cost'])
+    st.text('Login to see fantasy team stats')
 
-with tab1:
-    df, players = create_gameweek_df_for_team('total_points', gameweek_range, player_filter)
-    fig = plot_line_my_team_stats(df, players, 'Points')
-    st.plotly_chart(fig, use_container_width=True)
+else:
 
-with tab2:
-    df, players = create_gameweek_df_for_team('minutes', gameweek_range, player_filter)
-    fig = plot_line_my_team_stats(df, players, 'Minutes')
-    st.plotly_chart(fig, use_container_width=True)
+    # Siderbar position radio component
+    position_radio = st.radio(
+        label = "Position to Analyse",
+        options = ('Goalkeeper', 'Defender', 'Midfielder', 'Forward'), 
+        horizontal=True)
 
-with tab3:
-    df, players = create_gameweek_df_for_team('goals_scored', gameweek_range, player_filter)
-    fig = plot_line_my_team_stats(df, players, 'Goals Scored')
-    st.plotly_chart(fig, use_container_width=True)
+    # Sidebar gameweek slider
+    gameweek_range = st.slider(
+        label='Gameweeks to Analyse',
+        min_value = 1,
+        max_value = 38,
+        value = (1, 38))
 
-with tab4:
-    df, players = create_gameweek_df_for_team('assists', gameweek_range, player_filter)
-    fig = plot_line_my_team_stats(df, players, 'Assists')
-    st.plotly_chart(fig, use_container_width=True)
+    # Collect and filter player data
+    players_df = read_csv('data/players.csv')
+    player_filter = players_df[players_df['position'] == position_radio]['second_name'].to_list()
 
-with tab5:
-    df, players = create_gameweek_df_for_team('clean_sheets', gameweek_range, player_filter)
-    fig = plot_line_my_team_stats(df, players, 'Clean Sheets')
-    st.plotly_chart(fig, use_container_width=True)
+    # Configure page tabs
+    tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(['GW Points', 'GW Minutes', 'GW Goals', 'GW Assists', 
+                                            'GW CS', 'GW Cost'])
 
-with tab6:
-    df, players = create_gameweek_df_for_team('value', gameweek_range, player_filter)
-    fig = plot_line_my_team_stats(df, players, 'Cost (M)')
-    st.plotly_chart(fig, use_container_width=True)
+    with tab1:
+        df, players = create_gameweek_df_for_team('total_points', gameweek_range, player_filter)
+        fig = plot_line_my_team_stats(df, players, 'Points')
+        st.plotly_chart(fig, use_container_width=True)
+
+    with tab2:
+        df, players = create_gameweek_df_for_team('minutes', gameweek_range, player_filter)
+        fig = plot_line_my_team_stats(df, players, 'Minutes')
+        st.plotly_chart(fig, use_container_width=True)
+
+    with tab3:
+        df, players = create_gameweek_df_for_team('goals_scored', gameweek_range, player_filter)
+        fig = plot_line_my_team_stats(df, players, 'Goals Scored')
+        st.plotly_chart(fig, use_container_width=True)
+
+    with tab4:
+        df, players = create_gameweek_df_for_team('assists', gameweek_range, player_filter)
+        fig = plot_line_my_team_stats(df, players, 'Assists')
+        st.plotly_chart(fig, use_container_width=True)
+
+    with tab5:
+        df, players = create_gameweek_df_for_team('clean_sheets', gameweek_range, player_filter)
+        fig = plot_line_my_team_stats(df, players, 'Clean Sheets')
+        st.plotly_chart(fig, use_container_width=True)
+
+    with tab6:
+        df, players = create_gameweek_df_for_team('value', gameweek_range, player_filter)
+        fig = plot_line_my_team_stats(df, players, 'Cost (M)')
+        st.plotly_chart(fig, use_container_width=True)
--- pages/functions/ui_components.py
+++ pages/functions/ui_components.py
@@ -1,16 +1,31 @@
 import streamlit as st
 import numpy as np
 
-def position_checkbox(key):
+def position_checkbox(key, vertical=True):
     
     # Numpy array of positions
     filter_list = np.array(['Goalkeeper', 'Defender', 'Midfielder', 'Forward'])
 
-    # Streamlit sidebar checkbox components
-    goal_check = st.sidebar.checkbox('Goalkeeper', value=True, key=key+'goal')
-    def_check = st.sidebar.checkbox('Defender', value=True, key=key+'def')
-    mid_check = st.sidebar.checkbox('Midfielder', value=True, key=key+'mid')
-    fow_check = st.sidebar.checkbox('Forward', value=True, key=key+'fow')
+    if vertical:
+
+        # Streamlit sidebar checkbox components
+        goal_check = st.sidebar.checkbox('Goalkeeper', value=True, key=key+'goal')
+        def_check = st.sidebar.checkbox('Defender', value=True, key=key+'def')
+        mid_check = st.sidebar.checkbox('Midfielder', value=True, key=key+'mid')
+        fow_check = st.sidebar.checkbox('Forward', value=True, key=key+'fow')
+
+    else:
+        col1, col2, col3, col4 = st.columns(4)
+
+        with col1:
+            goal_check = st.checkbox('Goalkeeper', value=True, key=key+'goal')
+        with col2:
+            def_check = st.checkbox('Defender', value=True, key=key+'def')
+        with col3:
+            mid_check = st.checkbox('Midfielder', value=True, key=key+'mid')
+        with col4:
+            fow_check = st.checkbox('Forward', value=True, key=key+'fow')
+
     
     # Identify selected options
     filter_index= np.array([goal_check, def_check, mid_check, fow_check])
