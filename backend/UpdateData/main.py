1c116d957531ceb2329bbf220a7a7a201930ad65
rhys powell <rhys.powell2@btinternet.com>
2024-01-19 13:30:55 +0000

    ip whitelist removed

--- backend/UpdateData/main.py
+++ backend/UpdateData/main.py
@@ -16,24 +16,34 @@ warnings.filterwarnings("ignore")
 
 # Configure Logger
 logger = logging.getLogger('BASIC')
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(message)s')
+log_handler = logging.StreamHandler()
+log_handler.setFormatter(formatter)
+logger.addHandler(log_handler)
 
 # Connect to Database
+logger.info('Connecting to Database...')
 cnxn, cursor = connect_to_database()
 logger.info('Connected to Database')
 
 # Collect and Update Player Data
+logger.info('Updating Player Data in SQL...')
 player_df = collect_player_data()
 update_player_table(cnxn, cursor, player_df)
 logger.info('Player Data updated in SQL')
 
 # Collect and Update League Data
+logger.info('Updating League Data in SQL...')
 league_df = collect_league_data()
 update_leagues_table(cnxn, cursor, league_df)
 logger.info('League Data updated in SQL')
 
 # Collect and Update Premier League Table Data
+logger.info('Updating Premier League Table Data in SQL...')
 premier_league_table = collect_premier_league_table()
 update_premier_league_table(cnxn, cursor, premier_league_table)
+logger.info('Premier League Table Data Updated in SQL')
 
 # Close SQL database connection
 cursor.close()
