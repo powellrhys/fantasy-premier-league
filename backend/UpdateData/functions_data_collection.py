8abc6f9f0cb421dfc63701ac14d0a5385aa8f8fe
Rhys Powell <55314665+powellrhys@users.noreply.github.com>
2024-09-12 15:21:29 +0100

    manager squad info written to sql (#14)
    * manager squad info written to sql
    
    * gameweek retriever function added
    
    * flake8 linting

--- backend/UpdateData/functions_data_collection.py
+++ backend/UpdateData/functions_data_collection.py
@@ -7,6 +7,21 @@ import os
 
 load_dotenv()
 
+def collect_current_gameweek():
+
+    # Endpoint url
+    url = 'https://fantasy.premierleague.com/api/bootstrap-static/'
+
+    # Read in data from endpoint
+    r = requests.get(url)
+    gameweeks = r.json()['events']
+
+    # Collect current gamewek
+    gw = [gw['id'] for gw in gameweeks if gw['is_current']][0]
+
+    return gw
+
+
 def collect_player_data():
 
     # Endpoint url
@@ -101,12 +116,13 @@ def collect_single_league_data(league_id):
     df = pd.DataFrame(json['standings']['results'])
 
     # Create chips used dataframe
-    chip_df = create_league_chip_dataframe(df)
+    chip_df = create_league_chip_dataframe(df) \
+        .rename(columns={'entry': 'chip_entry'})
 
     # Join league data with chips data
-    df = df.join(chip_df, lsuffix='entry', rsuffix='entry') \
-        .drop(columns=['entryentry'], axis=1) \
-        .rename(columns={'rank': 'league_rank'})
+    df = df.join(chip_df, lsuffix='entry', rsuffix='chip_entry') \
+        .rename(columns={'rank': 'league_rank'}) \
+        .drop(columns=['chip_entry'])
     df['league_name'] = json['league']['name']
 
     return df
@@ -115,13 +131,95 @@ def collect_league_data():
     league_ids = os.getenv('leagues').replace(' ', '').split(',')
 
     league_df = pd.DataFrame(columns=['id', 'event_total', 'player_name', 'league_rank', 'last_rank', 'rank_sort',
-                                      'total', 'entry_name', 'bench_boost',
+                                      'total', 'entry', 'entry_name', 'bench_boost',
                                       'free_hit', 'triple_c', 'league_name'])
     for league_id in league_ids:
         df = collect_single_league_data(league_id)
         league_df = pd.concat([league_df, df])
 
     return league_df
+
+def collect_manager_squad_data(cnxn, gameweek):
+
+    # Define query to extract data from the fpl_leage_data table
+    query = '''
+        select
+            entry,
+            player_name,
+            league_name
+        from
+            fpl_league_data
+    '''
+
+    # Execute sql query
+    manager_squad_df = pd.read_sql(query, cnxn)
+
+    # Collect list of unique manager ids
+    unique_managers = list(set(manager_squad_df['entry'].tolist()))
+
+    # Define empty squad dataframe
+    squad_df = pd.DataFrame(columns=['manager_id', 'squad'])
+
+    # Iterate through manager ids
+    for manager_id in unique_managers:
+
+        # Collect squad data for specific manager
+        single_squad_df = collect_single_manager_squad_data(cnxn, manager_id, gameweek)
+        squad_df = pd.concat([squad_df, single_squad_df])
+
+    # Join squad data to manager data
+    squad_df = squad_df.reset_index().drop(columns=['index'])
+    manager_squad_df = manager_squad_df \
+        .merge(squad_df, left_on='entry', right_on='manager_id', how='left') \
+        .drop(columns=['manager_id']) \
+        .rename(columns={
+            'player_name': 'manager_name',
+            'squad': 'player_name'
+        })
+
+    return manager_squad_df
+
+
+def collect_single_manager_squad_data(cnxn, manager_id, gw):
+
+    # Define gameweek squad url
+    url = f'https://fantasy.premierleague.com/api/entry/{manager_id}/event/{gw}/picks/'
+
+    # Collect data from endpoint
+    r = requests.get(url)
+    json = r.json()
+
+    # Iterate through squad picks - collect player ids
+    pick_ids = [pick['element'] for pick in json['picks']]
+
+    # Convert ids to player names
+    squad = [collect_player_name_by_id(cnxn, pick_id) for pick_id in pick_ids]
+
+    # Create squad dataframe
+    squad_df = pd.DataFrame(
+        {'manager_id': [manager_id] * 15,
+         'squad': squad})
+
+    return squad_df
+
+
+def collect_player_name_by_id(cnxn, player_id):
+
+    # Define query
+    query = f'''
+        select
+            second_name
+        from
+            fpl_player_data
+        where
+            id = {player_id}
+    '''
+
+    # Collect player name from table
+    player_name = pd.read_sql(query, cnxn).iloc[0]['second_name']
+
+    return player_name
+
 
 def collect_premier_league_table():
 
