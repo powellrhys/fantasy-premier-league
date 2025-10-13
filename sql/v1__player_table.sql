4ffa9fd465aab6e9074313d69bc17d27cb4290bd
rhys powell <rhys.powell2@btinternet.com>
2025-10-13 20:16:29 +0100

    testing flyway

---
+++ sql/v1__player_table.sql
@@ -0,0 +1,26 @@
+USE [fantasy-premier-league]
+
+CREATE TABLE player (
+    id INT PRIMARY KEY,
+    second_name VARCHAR(100),
+    team VARCHAR(100),
+    element_type VARCHAR(50),
+    selected_by_percent DECIMAL(5,2),
+    now_cost INTEGER,
+    minutes INTEGER,
+    transfers_in INTEGER,
+    value_season DECIMAL(6,2),
+    total_points INTEGER,
+    goals_scored INTEGER,
+    assists INTEGER,
+    clean_sheets INTEGER,
+    goals_conceded INTEGER,
+    own_goals INTEGER,
+    penalties_saved INTEGER,
+    penalties_missed INTEGER,
+    yellow_cards INTEGER,
+    red_cards INTEGER,
+    saves INTEGER,
+    starts INTEGER,
+    position VARCHAR(50)
+);
