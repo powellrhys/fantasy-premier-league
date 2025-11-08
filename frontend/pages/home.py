e030c25233d879bbc92b258dbc4d807d3b547174
Rhys Powell <55314665+powellrhys@users.noreply.github.com>
2025-11-08 19:51:09 +0000

    Sql server maintenance scripts added (#26)
    * sql server start up function added
    
    * codecov related changes
    
    * flake8 linting

--- frontend/pages/home.py
+++ frontend/pages/home.py
@@ -1,16 +1,18 @@
 # Import python and project dependencies
 from frontend.pages.frontend_sections.home_page import render_home_page
 from streamlit_components.ui_components import configure_page_config
-from functions.data import collect_player_data
+from functions.data import collect_player_data, wake_up_database
 
 # Set page config
 configure_page_config(repository_name='fantasy-premier-league',
                       page_icon=":soccer:",
                       authentication_enabled=False)
 
+# Ensure database is online
+if wake_up_database():
 
-# Read player data from external source
-df = collect_player_data()
+    # Read player data from external source
+    df = collect_player_data()
 
-# Render home page section
-render_home_page(df=df)
+    # Render home page section
+    render_home_page(df=df)
